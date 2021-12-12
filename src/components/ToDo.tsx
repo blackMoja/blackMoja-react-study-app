56128eb6d95e02abcc91af87d5475fcb51eecc29
blackMoja <limlim980625@gmail.com>
2021-12-12 14:33:25 +0900

    feat: 간단한 TODO 예제

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,13 +1,47 @@
-import type { FC } from 'react';
+import { useSetRecoilState } from 'recoil';
+import { toDoState } from 'atoms';
+
+import type { FC, MouseEvent } from 'react';
 import type { ToDoType } from 'atoms';
 
-const ToDo: FC<ToDoType> = ({ text }) => {
+const ToDo: FC<ToDoType> = ({ text, category, id }) => {
+  const setToDos = useSetRecoilState(toDoState);
+
+  const onClick = (event: MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+
+    setToDos(oldToDos => {
+      const targetIndex = oldToDos.findIndex(toDo => toDo.id === id);
+      const newToDo = { text, id, category: name as ToDoType['category'] };
+
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
+  };
+
   return (
     <li>
       <span>{text}</span>
-      <button>To DO</button>
-      <button>Doing</button>
-      <button>Done</button>
+      {category !== 'DOING' && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== 'TO_DO' && (
+        <button name="TO_DO" onClick={onClick}>
+          To DO
+        </button>
+      )}
+      {category !== 'DONE' && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
     </li>
   );
 };
