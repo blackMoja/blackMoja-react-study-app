2f2404425b7fb03b96c48f7c913087235e096645
blackMoja <limlim980625@gmail.com>
2021-12-18 22:18:34 +0900

    feat: draggable example code

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,74 @@
-import { useRecoilState } from 'recoil';
-import { hourSelector, minuteState } from 'atoms';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import styled from 'styled-components';
 
-import type { FC, FormEvent } from 'react';
+import type { FC } from 'react';
+
+const Wrapper = styled.div`
+  display: flex;
+  max-width: 480px;
+  width: 100%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Boards = styled.div`
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  width: 100%;
+`;
+
+const Board = styled.div`
+  padding: 20px 10px;
+  padding-top: 30px;
+  background-color: ${props => props.theme.boardColor};
+  border-radius: 5px;
+  min-height: 200px;
+`;
+
+const Card = styled.div`
+  background-color: ${props => props.theme.cardColor};
+  border-radius: 5px;
+  padding: 10px 10px;
+  margin-bottom: 5px;
+`;
+
+const toDos = ['a', 'b', 'c', 'e', 'f'];
 
 const App: FC = () => {
-  const [minutes, setMinutes] = useRecoilState(minuteState);
-  const [hours, setHours] = useRecoilState(hourSelector);
-
-  const handleMinutesChange = (event: FormEvent<HTMLInputElement>) => {
-    setMinutes(Number(event.currentTarget.value));
-  };
-
-  const handleHoursChange = (event: FormEvent<HTMLInputElement>) => {
-    setHours(Number(event.currentTarget.value));
-  };
+  const handleDragEnd = () => {};
 
   return (
-    <div>
-      <input
-        value={minutes}
-        onChange={handleMinutesChange}
-        type="number"
-        placeholder="minute"
-      />
-      <input
-        value={hours}
-        onChange={handleHoursChange}
-        type="number"
-        placeholder="hours"
-      />
-    </div>
+    <DragDropContext onDragEnd={handleDragEnd}>
+      <Wrapper>
+        <Boards>
+          <Droppable droppableId="one">
+            {droppableOptions => (
+              <Board
+                ref={droppableOptions.innerRef}
+                {...droppableOptions.droppableProps}
+              >
+                {toDos.map((toDo, index) => (
+                  <Draggable draggableId={toDo} index={index}>
+                    {draggableOptions => (
+                      <Card
+                        ref={draggableOptions.innerRef}
+                        {...draggableOptions.draggableProps}
+                        {...draggableOptions.dragHandleProps}
+                      >
+                        {toDo}
+                      </Card>
+                    )}
+                  </Draggable>
+                ))}
+                {droppableOptions.placeholder}
+              </Board>
+            )}
+          </Droppable>
+        </Boards>
+      </Wrapper>
+    </DragDropContext>
   );
 };
 
