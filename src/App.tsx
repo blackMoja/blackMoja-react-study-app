3beef6a4cc8df34e14d0341c8d5d56a8d4ce3089
blackMoja <limlim980625@gmail.com>
2021-12-21 00:44:35 +0900

    feat: motion variants example code

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
 import type { FC } from 'react';
+import type { Variants } from 'framer-motion';
 
 const Wrapper = styled.div`
   display: flex;
@@ -13,28 +14,64 @@ const Wrapper = styled.div`
 `;
 
 const Box = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 15px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const myVars = {
+const Circle = styled(motion.div)`
+  height: 70px;
+  width: 70px;
+
+  border-radius: 35px;
+  place-self: center;
+  background-color: white;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants: Variants = {
   start: {
-    scale: 0,
+    opacity: 0,
+    scale: 0.5,
   },
   end: {
+    opacity: 1,
     scale: 1,
-    rotate: 360,
-    transition: { type: 'spring', delay: 0.5 },
+    transition: {
+      type: 'spring',
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.5,
+    },
+  },
+};
+
+const circleVariants: Variants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
   },
 };
 
 const App: FC = () => {
   return (
     <Wrapper>
-      <Box variants={myVars} initial="start" animate="end" />
+      <Box variants={boxVariants} initial="start" animate="end">
+        {/* 부모 Element의 initial, animate 옵션을 하위에서 받기 때문에 variants를 선언한 Object의 내부 인터페이스만 맞추면 됩니다. */}
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
     </Wrapper>
   );
 };
