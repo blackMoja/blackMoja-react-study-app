3a071a242855cf937b1fc2bfa9156184e04e61c7
blackMoja <limlim980625@gmail.com>
2021-12-20 22:09:14 +0900

    feat: simple box animation

--- src/App.tsx
+++ src/App.tsx
@@ -12,19 +12,23 @@ const Wrapper = styled.div`
   width: 100vw;
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
-  border-radius: 10px;
+  border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 const App: FC = () => {
   return (
     <Wrapper>
-      <Box />
-      <motion.div></motion.div>
+      {/* initial props : 애니메이션을 시작하는 방식을 명시하는 props = Element의 초기 상태 */}
+      <Box
+        transition={{ type: 'spring', bounce: 0.5 }}
+        initial={{ scale: 0 }}
+        animate={{ scale: 1, rotate: 360 }}
+      />
     </Wrapper>
   );
 };
