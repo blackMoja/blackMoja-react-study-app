33b86aabc333f86fc6ed992d615e7c095513ef52
blackMoja <limlim980625@gmail.com>
2022-01-23 21:19:09 +0900

    feat: motionValue 예제

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,22 @@
-import { useRef } from 'react';
 import styled from 'styled-components';
-import { motion } from 'framer-motion';
+import {
+  motion,
+  useMotionValue,
+  useTransform,
+  useViewportScroll,
+} from 'framer-motion';
 
-import type { FC } from 'react';
-import type { Variants } from 'framer-motion';
+import { FC, useEffect } from 'react';
 
-const Wrapper = styled.div`
+const Wrapper = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
 
-  height: 100vh;
+  height: 500vh;
   width: 100vw;
-`;
 
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.4);
-  border-radius: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
+  background-color: linear-gradient(135deg, #e09, #d0e);
 `;
 
 const Box = styled(motion.div)`
@@ -33,28 +27,24 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants: Variants = {
-  hover: { scale: 1.5, rotateZ: 90 },
-  click: { scale: 1, borderRadius: '100px' },
-  drag: { backgroundColor: 'rgb(251, 197, 49)', transition: { duration: 10 } },
-};
-
 const App: FC = () => {
-  const biggerBoxRef = useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0);
+  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+  const gradient = useTransform(
+    x,
+    [-800, 800],
+    [
+      'linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))',
+      'linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))',
+    ]
+  );
+  const { scrollYProgress } = useViewportScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
 
   return (
-    <Wrapper>
-      <BiggerBox ref={biggerBoxRef}>
-        <Box
-          variants={boxVariants}
-          whileHover="hover"
-          whileTap="click"
-          whileDrag="drag"
-          drag
-          dragElastic={0.5} // 기본값 0.5
-          dragConstraints={biggerBoxRef} // 드래그 영역 제안하는 props
-        />
-      </BiggerBox>
+    <Wrapper style={{ background: gradient }}>
+      <button onClick={() => x.set(200)}>click me.</button>
+      <Box style={{ x, rotateZ, scale }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 };
