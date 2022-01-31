a4a83f136833b9117ad6d7c41f864e4d014cb14c
blackMoja <limlim980625@gmail.com>
2022-02-01 00:30:35 +0900

    feat: 기본적인 Router 구조 설계

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from 'routes/Home';
+import Tv from 'routes/Tv';
+import Search from 'routes/Search';
+import Header from 'components/Header';
+
 import type { FC } from 'react';
 
 const App: FC = () => {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/tv" element={<Tv />}></Route>
+        <Route path="/search" element={<Search />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
